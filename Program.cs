f39a3f2a5c9b7e30017c0d632b3f2f7450112991
Xiaoyu <732004709@qq.com>
2022-10-11 20:25:54 +0800

    add stealth min js

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ await using var browser = await playwright.Chromium.LaunchAsync(new() { Channel 
 var context = await browser.NewContextAsync(new BrowserNewContextOptions() { StorageStatePath = @"N:\.NET CORE\PlaywrightDemo\auth.json" });
 
 var page = await context.NewPageAsync();
-await page.AddInitScriptAsync(scriptPath: @"N:\.NET CORE\Xiudong-Playwright\Xiudong-Playwright\stealth.min.js");
+await page.AddInitScriptAsync(scriptPath: @"N:\.NET CORE\PlaywrightDemo\stealth.min.js");
 await page.GotoAsync("https://wap.showstart.com/pages/passport/login/login?redirect=%2Fpages%2FmyHome%2FmyHome");
 
 var helper = new Helper(page, "0fc7521c6836ac036a39b1dabace907e", "182990", false, null, "1", 1);
